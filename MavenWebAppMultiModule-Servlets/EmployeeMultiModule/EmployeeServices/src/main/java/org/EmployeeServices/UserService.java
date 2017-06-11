f89c87838ce04cceb9223ec2cc54f624640355c1
sandeep <sandeep.madineni31@gmail.com>
2017-06-10 22:13:43 -0500

    Maven Servlets with Sessions and JDBC

---
+++ MavenWebAppMultiModule-Servlets/EmployeeMultiModule/EmployeeServices/src/main/java/org/EmployeeServices/UserService.java
@@ -0,0 +1,15 @@
+package org.EmployeeServices;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UserService {
+
+	public String getDateFromService(){
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		
+		return dateFormat.format(date);
+	}
+}
